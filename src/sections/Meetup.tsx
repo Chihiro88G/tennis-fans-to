97c5ab1d6903f1625000605c3489d0ccdb6c8eeb
Chihiro88G <1114chihiro@gmail.com>
2025-04-12 15:38:31 -0400

    link to meetup event groups

--- src/sections/Meetup.tsx
+++ src/sections/Meetup.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import '../App.css';
 
 function Meetup() {
+  const clickSearch = () => {
+    window.open('https://www.meetup.com/find/?keywords=tennis&location=ca--on--toronto&source=GROUPS');
+  }
+
   return (
     <div className='meetup-section'>
       <img src="tennis-meetup-img.jpg" alt="tennis balls" />
       <h2 className='white-color'>Looking for Tennis Meetups?</h2>
       <div className='search-btn-div'>
-        <button>SEARCH</button>
+        <button onClick={clickSearch}>SEARCH</button>
       </div>
     </div>
   );
